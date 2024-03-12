98b3becb61252e1ed9c2fa173a757d6f7c436d3b
Lanceky <evanslance100@gmail.com>
2024-03-11 19:07:20 -0700

    2-print.py

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-
-# Using snake_case for variable names
-message = "Programming is like building a multilingual puzzle"
-
-# Using print function to display the message
-print(message)
-
-# Print an empty line to ensure the file ends with a newline
-print()
+print("Programming is like building a multilingual puzzle")  # Using print function to display the message
+print()  # Adding an empty print statement to end with a newline
 
