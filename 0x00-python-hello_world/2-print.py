72b1798464f0f9f4c9501f7d17c6d099ea1970d4
Lanceky <evanslance100@gmail.com>
2024-03-11 19:08:50 -0700

    2-print.py

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-print("Programming is like building a multilingual puzzle")  # Using print function to display the message
-print()  # Adding an empty print statement to end with a newline
-
+print("Programming is like building a multilingual puzzle") 
