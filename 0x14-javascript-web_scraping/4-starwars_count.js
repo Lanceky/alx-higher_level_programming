1eef622500ee5413a4e3e4a48eedeaeb3fab2d7b
LAnce <evanslance100@gmail.co>
2024-07-30 07:00:33 -0700

    removed whitespace

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -11,18 +11,39 @@ if (process.argv.length !== 3) {
 // API URL from the command-line argument
 const apiUrl = process.argv[2];
 
-// Make GET request
+// Make GET request to fetch film details
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
-    const films = JSON.parse(body);
-    let count = 0;
-    films.results.forEach(film => {
-      if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        count++;
+    return;
+  }
+
+  const films = JSON.parse(body);
+  let count = 0;
+
+  // Helper function to process each film
+  const processFilm = (film, callback) => {
+    const characters = film.characters || [];
+    const wedgeAntillesUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+    // Check if Wedge Antilles is in this film
+    const isInFilm = characters.some(characterUrl => characterUrl === wedgeAntillesUrl);
+    if (isInFilm) {
+      count++;
+    }
+    
+    callback();
+  };
+
+  // Process each film
+  let completedRequests = 0;
+  films.results.forEach(film => {
+    processFilm(film, () => {
+      completedRequests++;
+      // After processing all films, print the count
+      if (completedRequests === films.results.length) {
+        console.log(count);
       }
     });
-    console.log(count);
-  }
+  });
 });
