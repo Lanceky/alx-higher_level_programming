4288f9bd0197f4f19b0502145f4b3c94a4dae89f
LAnce <evanslance100@gmail.co>
2024-07-30 07:06:47 -0700

    added 4-starwars-count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -11,39 +11,18 @@ if (process.argv.length !== 3) {
 // API URL from the command-line argument
 const apiUrl = process.argv[2];
 
-// Make GET request to fetch film details
+// Make GET request
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
-    return;
-  }
-
-  const films = JSON.parse(body);
-  let count = 0;
-
-  // Helper function to process each film
-  const processFilm = (film, callback) => {
-    const characters = film.characters || [];
-    const wedgeAntillesUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
-
-    // Check if Wedge Antilles is in this film
-    const isInFilm = characters.some(characterUrl => characterUrl === wedgeAntillesUrl);
-    if (isInFilm) {
-      count++;
-    }
-    
-    callback();
-  };
-
-  // Process each film
-  let completedRequests = 0;
-  films.results.forEach(film => {
-    processFilm(film, () => {
-      completedRequests++;
-      // After processing all films, print the count
-      if (completedRequests === films.results.length) {
-        console.log(count);
+  } else {
+    const films = JSON.parse(body);
+    let count = 0;
+    films.results.forEach(film => {
+      if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count++;
       }
     });
-  });
+    console.log(count);
+  }
 });
