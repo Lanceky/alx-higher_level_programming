be32cb2ea12acf7d67b84aaa981c7cb62df312aa
Lance <evanslance100@gmail.com>
2024-08-18 01:56:55 -0700

    condition for exe

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-args = sys.argv[1:]
-num_args = len(args)
-
-if num_args == 0:
-    print("0 arguments.")
-elif num_args == 1:
-    print("1 argument:")
-else:
-    print(f"{num_args} arguments:")
-
-for i, arg in enumerate(args, start=1):
-    print(f"{i}: {arg}")
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print(f"{argc} arguments:")
+    
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{i}: {arg}")
