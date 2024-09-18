c124cdb100a0782cafad2feab28ddb1791c62428
Lanceky <evanslance100@gmail.com>
2024-09-18 09:06:54 -0700

    refactor 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,65 @@
-#!/usr/bin/python3
-import sys
+#include <Python.h>
+#include <stdio.h>
 
-def safe_print_integer_err(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError) as e:
-        print("Exception: {}".format(str(e)), file=sys.stderr)
-        return False
+void print_python_list(PyObject *p) {
+    Py_ssize_t size, i;
+    PyObject *item;
+
+    if (!PyList_Check(p)) {
+        fprintf(stderr, "[ERROR] Invalid List Object\n");
+        return;
+    }
+
+    size = ((PyVarObject *)p)->ob_size;
+
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++) {
+        item = ((PyListObject *)p)->ob_item[i];
+        printf("Element %ld: %s\n", i, item->ob_type->tp_name);
+        if (PyBytes_Check(item)) {
+            print_python_bytes(item);
+        } else if (PyFloat_Check(item)) {
+            print_python_float(item);
+        }
+    }
+}
+
+void print_python_bytes(PyObject *p) {
+    Py_ssize_t size, i;
+    char *str;
+
+    if (!PyBytes_Check(p)) {
+        fprintf(stderr, "[ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
+    size = ((PyVarObject *)p)->ob_size;
+    str = ((PyBytesObject *)p)->ob_sval;
+
+    printf("[.] bytes object info\n");
+    printf("  size: %ld\n", size);
+    printf("  trying string: %s\n", str);
+
+    printf("  first %ld bytes:", (size < 10) ? size + 1 : 10);
+    for (i = 0; i < size && i < 10; i++) {
+        printf(" %02x", (unsigned char)str[i]);
+    }
+    printf("\n");
+}
+
+void print_python_float(PyObject *p) {
+    double value;
+
+    if (!PyFloat_Check(p)) {
+        fprintf(stderr, "[ERROR] Invalid Float Object\n");
+        return;
+    }
+
+    value = ((PyFloatObject *)p)->ob_fval;
+
+    printf("[.] float object info\n");
+    printf("  value: %f\n", value);
+}
