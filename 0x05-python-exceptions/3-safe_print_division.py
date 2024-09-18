e1a968c9aae9e5c571b235a4e27cc4115548daf2
Lanceky <evanslance100@gmail.com>
2024-09-18 04:57:52 -0700

    betty to be proud

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def safe_print_division(a, b);
+
+def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDIvisionError:
+    except ZeroDivisionError:
         result = None
     finally:
-        print("Iniside result: {}".format(result))
+        print("Inside result: {}".format(result))
     return result
