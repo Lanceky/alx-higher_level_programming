7c06c8c6772afa68e614d90fa7574ea8a649aa9d
Lance <evanslance100@gmail.com>
2024-09-23 15:33:53 -0700

    refactored 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,20 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function() {
-    this.value += 1;
-  }
+  value: 12
 };
 
 console.log(myObject);
+
+myObject.incr = function() {
+  this.value += 1;
+};
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
