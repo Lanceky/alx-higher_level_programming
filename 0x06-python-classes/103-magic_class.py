138f839c75904ceebeb44cfa3afa320ff91f9333
Lanceky <evanslance100@gmail.com>
2024-09-23 12:47:38 -0700

    created 103-magic_class.py that does exactly the same as a Python bytecode:

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module: 103-magic_class.py
+Defines the MagicClass class that emulates the given Python bytecode.
+"""
+import math
+
+class MagicClass:
+    """
+    Represents a MagicClass that emulates given Python bytecode.
+    """
+    def __init__(self, radius=0):
+        self.radius = 0
+        self.__radius = 0
+        self
+
