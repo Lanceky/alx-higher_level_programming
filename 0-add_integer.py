53542530f5744e678d022064baf19efcf7bdd079
Lanceky <evanslance100@gmail.com>
2024-09-26 12:06:03 -0700

    created a file and a test case

---
+++ 0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module provides a function to add two integers.
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float, optional): The second number. Defaults to 98.
+
+    Returns:
+        int: The addition of a and b.
+
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    return int(a) + int(b)
---
+++ README.md
@@ -0,0 +1,3 @@
+Python - Test-driven development
+This directory contains projects that test python scripts
+Main concpt - UNITTEST & DOCTESTS
